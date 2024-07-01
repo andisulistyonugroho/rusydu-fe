e15813403e6e57d9cd14ec3fe271395dbf9d1bd7
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-07-01 13:59:13 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config"
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    'vuetify-nuxt-module'
+  ],
+  vuetify: {
+    moduleOptions: {
+    },
+    vuetifyOptions: {
+      
+    }
+  }
+})

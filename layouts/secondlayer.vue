8afc71211de6206557a66c22ac92c038fa527f7f
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-07-23 17:05:22 +0700

    style: second layer

---
+++ layouts/secondlayer.vue
@@ -0,0 +1,31 @@
+<script setup>
+const waitDialog = ref(false)
+const drawer = ref(false)
+const snacko = ref({
+  message: null,
+  color: null,
+  open: false,
+})
+</script>
+<template>
+  <v-app>
+    <v-app-bar class="border-b">
+      <v-btn icon @click="$router.go(-1)"><v-icon>mdi-arrow-left</v-icon></v-btn>
+      <v-app-bar-title>Rusydu</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <slot />
+    </v-main>
+
+    <v-snackbar v-model="snacko.open" :color="snacko.color" multi-line>
+      {{ snacko.message }}
+
+      <template v-slot:actions>
+        <v-btn :color="snacko.color" variant="text" @click="snacko.open = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-app>
+</template>

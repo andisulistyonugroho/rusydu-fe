9cd21e4b8e456bb6f71c5f8db07dccb4827962f8
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-07-30 04:10:32 +0700

    refactor: remove function to generate input

--- stores/record.ts
+++ stores/record.ts
@@ -2,46 +2,22 @@ export const useRecordStore = defineStore('record', () => {
   const { $api } = useNuxtApp()
 
   const addRecord = (async (payload: {
-    title: string, amount: number, tCode: string, tDate: string,
-    fromFinancialAccountId: number, toFinancialAccountId: number
+    title: string, tCode: string, amount: number,
+    fromFinancialAccountId: number, toFinancialAccountId: number,
+    tDate: string,
   }) => {
     try {
-
-      const generatedInput = generateInput({
-        tCode: payload.tCode, amount: payload.amount,
-        fromFinancialAccountId: payload.fromFinancialAccountId,
-        toFinancialAccountId: payload.toFinancialAccountId
-      })
-
       const { data } = await $api.post('/FinancialRecords', {
         title: payload.title,
-        amountIn: generatedInput.amountIn,
-        amountOut: generatedInput.amountOut,
-        financialAccountId: generatedInput.financeAccountId
+        amountIn: payload.tCode === 'C' ? payload.amount : 0,
+        amountOut: payload.tCode === 'D' ? payload.amount : 0,
+        tCode: payload.tCode,
+        tDate: payload.tDate,
+        financialAccountId: payload.tCode === 'C' ? payload.toFinancialAccountId : payload.fromFinancialAccountId
       })
-      return Promise.resolve(data)
+      return Promise.resolve(true)
     } catch (error) {
       return Promise.reject(error)
-    }
-  })
-
-  const generateInput = ((input: { tCode: string, amount: number, fromFinancialAccountId: number, toFinancialAccountId: number }) => {
-    let amountIn = 0
-    let amountOut = 0
-    let financeAccountId = null
-
-    if (input.tCode === 'C') {
-      amountIn = input.amount
-      financeAccountId = input.toFinancialAccountId
-    } else if (input.tCode === 'D') {
-      amountOut = input.amount
-      financeAccountId = input.fromFinancialAccountId
-    }
-
-    return {
-      amountIn: amountIn,
-      amountOut: amountOut,
-      financeAccountId: financeAccountId
     }
   })
 
